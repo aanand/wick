fba89c29cff05d0e389e27bc0b68ff782777e60a
Aanand Prasad <aanand.prasad@gmail.com>
2013-03-18 18:51:06 +0000

    Move output stuff to its own class

--- lib/irc_client.rb
+++ lib/irc_client.rb
@@ -1,4 +1,5 @@
 require 'irc_client/session'
+require 'irc_client/output/basic'
 require 'irc_client/runner/socket'
 require 'stream'
 
@@ -8,19 +9,14 @@ module IRCClient
   def self.start(options)
     session = Session.new
 
-    session.network_in.map { |data|
-      data.strip.each_line.map { |line| "< #{line.strip}".black.bold }
-    }.pipe(session.user_out)
-
-    session.network_out.map { |data|
-      data.strip.each_line.map { |line| "> #{line.strip}".black.bold }
-    }.pipe(session.user_out)
-
     session.user_in.pipe(session.network_out)
 
     connection_start = session.network_in.filter { |line| line == "CONNECTION_START" }
     nick_and_user_msgs = connection_start.map { |line| "NICK frippery\nUSER frippery () * FRiPpery" }
     nick_and_user_msgs.pipe(session.network_out)
+
+    output = Output::Basic.new
+    output.start(session)
 
     runner = Runner::Socket.new(options)
     runner.start(session)
---
+++ lib/irc_client/output/basic.rb
@@ -0,0 +1,15 @@
+module IRCClient
+  module Output
+    class Basic
+      def start(session)
+        session.network_in.map { |data|
+          data.strip.each_line.map { |line| "< #{line.strip}".black.bold }
+        }.pipe(session.user_out)
+
+        session.network_out.map { |data|
+          data.strip.each_line.map { |line| "> #{line.strip}".black.bold }
+        }.pipe(session.user_out)
+      end
+    end
+  end
+end
