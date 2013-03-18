d050a6da64afe0d342a1efafa61c3c85e81bd6b2
Aanand Prasad <aanand.prasad@gmail.com>
2013-03-18 16:37:07 +0000

    IO.select loop, four streams for network + user read/write

--- lib/irc_client.rb
+++ lib/irc_client.rb
@@ -1,28 +1,72 @@
 require 'socket'
 require 'stream'
 
-class IRCClient
+module IRCClient
   def self.start(options)
-    new(options).start
+    Session.new(options).start
   end
 
-  def initialize(options)
-    incoming = Stream.new
+  class Session
+    def initialize(options)
+      network_in  = Stream.new
+      network_out = Stream.new
+      user_in     = Stream.new
+      user_out    = Stream.new
 
-    incoming.each do |line|
-      puts "Got line: #{line.inspect}"
+      network_in.each do |line|
+        user_out << "Got line from network: #{line.inspect}"
+      end
+
+      user_in.each do |line|
+        user_out << "Got line from user input: #{line.inspect}"
+      end
+
+      options = options.merge(
+        network_in:  network_in,
+        network_out: network_out,
+        user_in:     user_in,
+        user_out:    user_out
+      )
+
+      Runner::Socket.new(options).start
     end
-
-    open_socket(options[:host], options[:port], incoming)
   end
 
-  def open_socket(host, port, incoming)
-    s = TCPSocket.new(host, port)
+  module Runner
+    class Socket
+      def initialize(options)
+        @network_in  = options.fetch(:network_in)
+        @network_out = options.fetch(:network_out)
+        @user_in     = options.fetch(:user_in)
+        @user_out    = options.fetch(:user_out)
 
-    while line = s.gets
-      incoming << line
+        @network_io  = TCPSocket.new(options.fetch(:host), options.fetch(:port))
+        @user_in_io  = options.fetch(:user_in_io)  { $stdin  }
+        @user_out_io = options.fetch(:user_out_io) { $stdout }
+      end
+
+      def start
+        @network_out.each do |line|
+          @network_io.puts(line)
+        end
+
+        @user_out.each do |line|
+          @user_out_io.puts(line)
+        end
+
+        while true
+          reads, writes, errors = IO.select([@network_io, @user_in_io])
+          reads.each do |io|
+            if io == @network_io
+              @network_in << io.read_nonblock(1_000_000)
+            elsif io == @user_in_io
+              @user_in << io.read_nonblock(1_000_000)
+            end
+          end
+        end
+
+        network.close
+      end
     end
-
-    s.close
   end
 end
