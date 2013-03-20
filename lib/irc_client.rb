60866e6058862393e3c9715edfc4f80a4a457613
Aanand Prasad <aanand.prasad@gmail.com>
2013-03-20 18:17:04 +0000

    Move message/command parsing out of Client

--- lib/irc_client.rb
+++ lib/irc_client.rb
@@ -1,11 +1,17 @@
 require 'stream'
 
+require 'irc_client/message'
+require 'irc_client/user_command'
+
 module IRCClient
   def self.start!(client, output, runner)
     network_in  = Stream.new
     user_in     = Stream.new
 
-    network_out = client.transform(network_in, user_in)
+    messages      = network_in.map { |line| Message.parse(line)     }
+    user_commands = user_in.map    { |line| UserCommand.parse(line) }
+
+    network_out = client.transform(messages, user_commands)
     user_out    = output.transform(network_in, network_out)
 
     runner.listen!(network_in, network_out, user_in, user_out)
--- lib/irc_client/client.rb
+++ lib/irc_client/client.rb
@@ -1,20 +1,13 @@
 require 'stream'
 
-require 'irc_client/message'
-require 'irc_client/user_command'
-
 module IRCClient
   class Client
-    def transform(network_in, user_in)
-      messages = network_in.map { |line| Message.parse(line) }
-
+    def transform(messages, user_commands)
       connection_start = messages.filter { |msg| msg.command == "CONNECTION_START" }
       nick_and_user_msgs = connection_start.flat_map { |_| Stream.from_array(["NICK frippery", "USER frippery () * FRiPpery"]) }
 
       ping = messages.filter { |msg| msg.command == "PING" }
       pong = ping.map { |msg| "PONG " + msg.params.join(" ") }
-
-      user_commands = user_in.map { |line| UserCommand.parse(line) }
 
       current_channel = user_commands.filter { |cmd| cmd.action == :join }
                                      .map(&:argument)
